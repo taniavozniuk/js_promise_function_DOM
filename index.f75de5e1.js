3cf1517afdad39675a3e7353ce6eedfc0eafc5c7
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2024-11-29 20:13:11 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        element.addEventListener(eventName, ()=>{
+            const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            resolve(message);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const messageDiv = document.createElement("div");
+    messageDiv.className = "message";
+    messageDiv.textContent = message;
+    document.body.appendChild(messageDiv);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
